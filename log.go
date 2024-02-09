496b5cd46a03a3c6a793eacc8b219cd83ca757c5
efron licht <efron.dev@gmail.com>
2024-02-08 16:33:19 -0800

    simplify

--- log.go
+++ log.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
-	"os"
 	"runtime"
 	"runtime/debug"
 	"sync"
@@ -27,54 +26,15 @@ var (
 	logger *slog.Logger // cached logger instance. this is usually synonymous with slog.Default, but we cache it here to avoid the overhead of calling slog.Default repeatedly.
 )
 
-// Debug logs at LevelDebug with the given context, initializing the logger if necessary.
-func Debug(ctx context.Context, msg string, args ...any) {
-	Log().DebugContext(ctx, msg, args...)
-}
-
-// Info logs at LevelInfo with the given context, initializing the logger if necessary.
-func Info(ctx context.Context, msg string, args ...any) { Log().InfoContext(ctx, msg, args...) }
-
-// Warn logs at LevelWarn with the given context, initializing the logger if necessary. This is essentially an alias for Log().WarnContext.
-func Warn(ctx context.Context, msg string, args ...any) { Log().WarnContext(ctx, msg, args...) }
-
-// Error logs at LevelError with the given context, initializing the logger if necessary.
-func Error(ctx context.Context, msg string, args ...any) {
-	Log().ErrorContext(ctx, msg, args...)
-}
-
-// LogAttrs is a more efficient version of [.Log] that accepts only Attrs, initializing the logger if necessary.
-func LogAttrs(ctx context.Context, lvl slog.Level, msg string, attrs ...slog.Attr) {
-	Log().LogAttrs(ctx, lvl, msg, attrs...)
-}
-
-// With returns a Logger that includes the given attributes in each output operation. See the documentation for [log/slog.Logger.With].
-// Arguments are converted to attributes as if by [Logger.Log].
-// It initializes the logger if necessary.
-func With(args ...any) *slog.Logger { return Log().With(args...) }
-
-// WithGroup returns a Logger that includes the given group in each output operation. See the documentation for [Logger.WithGroup].
-// It initializes the logger if necessary.
-func WithGroup(group string) *slog.Logger { return Log().WithGroup(group) }
-
-// Log returns a handle to the initialized logger. All other functions in this package are just wrappers around this one.
-// The first call initializes the package: further calls return the same logger.
-func Log() *slog.Logger { once.Do(func() { initEager(nil, os.Stderr) }); return logger }
-
-// Initalize the package with one or more writers. This is optional: if you don't call it, the package will initialize itself with a default writer (os.Stderr)
-func Init(m *Metadata, writers ...io.Writer) {
-	once.Do(func() { initEager(m, writers...) })
-}
-
 // see buildmeta.go for the definition of Metadata
 type Metadata struct {
 	InstanceID, Service, Env            string
 	VCSName, VCSCommit, VCSTag, VCSTime string
 }
 
-// eagerly initialize the package. called exactly once by Log.
+// Initalize the package with one or more writers. This is optional: if you don't call it, the package will initialize itself with a default writer (os.Stderr)
 // it's OK to use nil for the metadata: this program will fill in on a best-effort basis.
-func initEager(m *Metadata, writers ...io.Writer) {
+func Init(m *Metadata, writers ...io.Writer) {
 	var w io.Writer
 	switch len(writers) {
 	case 0:
@@ -111,7 +71,8 @@ FILLED:
 	fmt.Println("rplog.initEager: found metadata", m)
 
 	jsonHandler := slog.NewJSONHandler(w, &slog.HandlerOptions{AddSource: true, Level: enve.FromTextOr("RUNPOD_LOG_LEVEL", slog.LevelInfo)})
-	logger = slog.New(&Handler{Handler: jsonHandler.WithAttrs([]slog.Attr{
+
+	slog.SetDefault(slog.New(&Handler{Handler: jsonHandler.WithAttrs([]slog.Attr{
 		slog.String("vcs_name", m.VCSName),
 		slog.String("vcs_commit", m.VCSCommit),
 		slog.String("vcs_tag", m.VCSTag),
@@ -120,9 +81,8 @@ FILLED:
 		slog.String("instance_id", m.InstanceID),
 		slog.String("service", m.Service),
 		slog.String("language_version", runtime.Version()),
-	})})
+	})}))
 
-	slog.SetDefault(logger)
 }
 
 // Handle the log record, adding the metadata to it (always) and the Trace (if it exists).
